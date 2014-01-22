39b0599cee042c4b9f30a61769f47fc5bdd388d9
Ryan Cain <cainr10@gmail.com>
2014-01-22 11:08:02 -0500

    who knows

--- gen/uf/seniorproj/alexryan/R.java
+++ gen/uf/seniorproj/alexryan/R.java
@@ -28,9 +28,15 @@ public final class R {
         public static final int ic_launcher=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int imageButton1=0x7f080000;
-        public static final int imageButton2=0x7f080001;
+        public static final int action_settings=0x7f080008;
+        public static final int imageButton1=0x7f080002;
+        public static final int imageButton2=0x7f080003;
+        public static final int imageButton3=0x7f080004;
+        public static final int imageButton4=0x7f080005;
+        public static final int imageButton5=0x7f080006;
+        public static final int imageButton6=0x7f080007;
+        public static final int linearLayout1=0x7f080000;
+        public static final int topLayout=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -41,6 +47,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int desc=0x7f050006;
         public static final int hello_alex=0x7f050004;
         public static final int hello_ryan=0x7f050003;
         public static final int hello_world=0x7f050002;
