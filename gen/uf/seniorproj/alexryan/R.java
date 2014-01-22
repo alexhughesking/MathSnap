1fff364e6e8b91068eac409155c6f14e03548868
Ryan Cain <cainr10@gmail.com>
2014-01-22 11:39:03 -0500

    single button

--- gen/uf/seniorproj/alexryan/R.java
+++ gen/uf/seniorproj/alexryan/R.java
@@ -28,15 +28,8 @@ public final class R {
         public static final int ic_launcher=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
-        public static final int imageButton1=0x7f080002;
-        public static final int imageButton2=0x7f080003;
-        public static final int imageButton3=0x7f080004;
-        public static final int imageButton4=0x7f080005;
-        public static final int imageButton5=0x7f080006;
-        public static final int imageButton6=0x7f080007;
-        public static final int linearLayout1=0x7f080000;
-        public static final int topLayout=0x7f080001;
+        public static final int action_settings=0x7f080001;
+        public static final int imageButton1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -45,7 +38,7 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int TestingMerge=0x7f050007;
+        public static final int TestingMerge=0x7f050008;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int asd=0x7f050007;
