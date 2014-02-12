be4891fe333fcf805acc3e5bb98daeabc982f3ac
Alex King <alexhugesking@gmail.com>
2014-02-12 10:56:37 -0500

    FOIL highlighting and new problems

--- src/uf/seniorproj/alexryan/FoilActivity.java
+++ src/uf/seniorproj/alexryan/FoilActivity.java
@@ -1,15 +1,13 @@
 package uf.seniorproj.alexryan;
 
 import android.app.Activity;
-import android.graphics.Point;
 import android.os.Bundle;
 import android.text.Html;
-import android.view.Display;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnFocusChangeListener;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,31 +18,38 @@ public class FoilActivity extends Activity {
 	EditText box3; //x
 	EditText box4; //constant
 	
+	TextView problem; //the problem at the top
 	TextView correctText; //correct or try again
 	LinearLayout focusStealer; //to remove focus from editText
 	
 	int a, b, c, d; //the coefficients for the problem
+	int[] aData = {1, 1, 1, 1, 1, 2, 2, 1, 2, 3};
+	int[] bData = {-1, 2, -2, 4, 1, 1, -1, -5, 7, -1};
+	int[] cData = {1, 1, 1, 1, 1, 1, 2, 2, 1, 2};
+	int[] dData = {2, 2, -3, -2, -1, 2, -3, 3, 4, -2};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_foil);
 		
+		Bundle extras = getIntent().getExtras();
+		int probNum = extras.getInt("probNum");//get the problem number that was clicked
+		
 		
 
 		
 		//Problem information
 		//Will likely be stored in array later
-		String problemData1 = "(x + 3)(x - 2)";
-		a = 1;
-		b = 3;
-		c = 1;
-		d = -2;
+		a = aData[probNum];
+		b = bData[probNum];
+		c = cData[probNum];
+		d = dData[probNum];
 		
 		//initialize
 		//Set the problem at the top
-		TextView problem = (TextView)findViewById(R.id.foilProblem);
-		problem.setText(problemData1);
+		problem = (TextView)findViewById(R.id.foilProblem);
+		setProblemText(0);//set the text at the top
 		
 		box1 = (EditText)findViewById(R.id.foilEditText1);
 		box2 = (EditText)findViewById(R.id.foilEditText2);
@@ -57,11 +62,181 @@ public class FoilActivity extends Activity {
 		TextView xSquared = (TextView)findViewById(R.id.foilTextView1);
 		xSquared.setText(Html.fromHtml("x<sup><small>2</small></sup> + "));
 		
+		//set focus listeners for changing text color
+		box1.setOnFocusChangeListener(new OnFocusChangeListener() {
+			
+			@Override
+			public void onFocusChange(View v, boolean hasFocus) {
+				if (hasFocus) {
+					setProblemText(1);
+				}
+				
+			}
+		});
 		
+		box2.setOnFocusChangeListener(new OnFocusChangeListener() {
+			
+			@Override
+			public void onFocusChange(View v, boolean hasFocus) {
+				if (hasFocus) {
+					setProblemText(2);
+				}
+				
+			}
+		});
+		
+		box3.setOnFocusChangeListener(new OnFocusChangeListener() {
+			
+			@Override
+			public void onFocusChange(View v, boolean hasFocus) {
+				if (hasFocus) {
+					setProblemText(3);
+				}
+				
+			}
+		});
+		
+		box4.setOnFocusChangeListener(new OnFocusChangeListener() {
+			
+			@Override
+			public void onFocusChange(View v, boolean hasFocus) {
+				if (hasFocus) {
+					setProblemText(4);
+				}
+				
+			}
+		});
+		
+		
+	}
+	
+	public void setProblemText(int boxNo) {
+		//boxNo is the editText with focus
+		//boxNo == 0 means initialize problem
+		String text = "(";
+		if (boxNo == 1) {
+			text = text + "<font color=\"lime\">";
+			if (a == 1)
+				text = text + "x ";
+			else
+				text = text + a + "x ";
+			text = text + "</font>";
+			if (b < 0)
+				text = text + "- " + (-1*b);
+			else
+				text = text + "+ " + b;
+			text = text + ")(";
+			text = text + "<font color=\"lime\">";
+			if (c == 1)
+				text = text + "x ";
+			else
+				text = text + c + "x ";
+			text = text + "</font>";
+			if (d < 0)
+				text = text + "- " + (-1*d);
+			else
+				text = text + "+ " + d;
+			text = text + ")";
+		}
+		else if (boxNo == 2) {
+			text = text + "<font color=\"lime\">";
+			if (a == 1)
+				text = text + "x ";
+			else
+				text = text + a + "x ";
+			text = text + "</font>";
+			if (b < 0)
+				text = text + "- " + (-1*b);
+			else
+				text = text + "+ " + b;
+			text = text + ")(";
+			if (c == 1)
+				text = text + "x ";
+			else
+				text = text + c + "x ";
+			text = text + "<font color=\"lime\">";
+			if (d < 0)
+				text = text + "- " + (-1*d);
+			else
+				text = text + "+ " + d;
+			text = text + "</font>";
+			text = text + ")";
+		}
+		else if(boxNo == 3) {
+			if (a == 1)
+				text = text + "x ";
+			else
+				text = text + a + "x ";
+			text = text + "<font color=\"lime\">";
+			if (b < 0)
+				text = text + "- " + (-1*b);
+			else
+				text = text + "+ " + b;
+			text = text + "</font>";
+			text = text + ")(";
+			text = text + "<font color=\"lime\">";
+			if (c == 1)
+				text = text + "x ";
+			else
+				text = text + c + "x ";
+			text = text + "</font>";
+			if (d < 0)
+				text = text + "- " + (-1*d);
+			else
+				text = text + "+ " + d;
+			text = text + ")";
+		}
+		else if (boxNo == 4) {
+			if (a == 1)
+				text = text + "x ";
+			else
+				text = text + a + "x ";
+			text = text + "<font color=\"lime\">";
+			if (b < 0)
+				text = text + "- " + (-1*b);
+			else
+				text = text + "+ " + b;
+			text = text + "</font>";
+			text = text + ")(";
+			if (c == 1)
+				text = text + "x ";
+			else
+				text = text + c + "x ";
+			text = text + "<font color=\"lime\">";
+			if (d < 0)
+				text = text + "- " + (-1*d);
+			else
+				text = text + "+ " + d;
+			text = text + "</font>";
+			text = text + ")";
+		}
+		else {
+			if (a == 1)
+				text = text + "x ";
+			else
+				text = text + a + "x ";
+			if (b < 0)
+				text = text + "- " + (-1*b);
+			else
+				text = text + "+ " + b;
+			text = text + ")(";
+			if (c == 1)
+				text = text + "x ";
+			else
+				text = text + c + "x ";
+			if (d < 0)
+				text = text + "- " + (-1*d);
+			else
+				text = text + "+ " + d;
+			text = text + ")";
+			
+		}
+		problem.setText(Html.fromHtml(text));
 	}
 	
 	public void checkAnswer(View v) {
 		focusStealer.requestFocus();//remove focus
+		setProblemText(0);//reset original problem
 		String answer1 = box1.getText().toString();
 		String answer2 = box2.getText().toString();
 		String answer3 = box3.getText().toString();
